ce944a425ef08277a1f20236be11dbf2d3f0e08a
LSF970 <Lfairbrass@spartaglobal.com>
2023-02-01 11:33:04 +0000

    Added content for loops

---
+++ ten_user_inputs.py
@@ -0,0 +1,25 @@
+# While loop
+
+# amount = 0
+# number = 0
+#
+# while True:
+#     number += int(input("Number: "))
+#     amount += 1
+#     if amount == 10:
+#         break
+#
+# print(f"Numbers in total: {amount}")
+# print(f"Sum of the numbers: {number}")
+# print(number)
+
+
+# For loop
+
+total = 0
+
+for i in range(3):
+    total += int(input("Enter a Number: "))
+
+print(f"the total of your inputs is: {total}")
+
